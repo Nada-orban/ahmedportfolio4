c508682fa26ae8e36f59bbb275f636e5e0280dab
nada <nadasamir9334@gmail.com>
2023-04-11 16:14:00 -0400

    every thing work'

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import Nav from '../../Components/Nav'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <> <Nav />
+      <Component {...pageProps} /></>
+
+  )
 }
